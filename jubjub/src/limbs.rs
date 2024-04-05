b8fd0069a12e471681bd72791608fd4a5334257e
ashWhiteHat <phantomofrotten@gmail.com>
2024-04-05 13:07:08 +0900

    jubjub: refactor with adc

--- jubjub/src/limbs.rs
+++ jubjub/src/limbs.rs
@@ -1,15 +1,13 @@
 extern crate alloc;
-use crate::math::sbb;
+use crate::math::{adc, sbb};
 use alloc::vec::Vec;
 
 #[inline(always)]
 pub(crate) const fn add(a: [u64; 4], b: [u64; 4], p: [u64; 4]) -> [u64; 4] {
     let s = a[0] as u128 + b[0] as u128;
     let (l0, c) = (s as u64, (s >> 64) as u64);
-    let s = a[1] as u128 + b[1] as u128 + c as u128;
-    let (l1, c) = (s as u64, (s >> 64) as u64);
-    let s = a[2] as u128 + b[2] as u128 + c as u128;
-    let (l2, c) = (s as u64, (s >> 64) as u64);
+    let (l1, c) = adc(a[1], b[1], c);
+    let (l2, c) = adc(a[2], b[2], c);
     let l3 = a[3].wrapping_add(b[3]).wrapping_add(c);
 
     let s = (l0 as u128).wrapping_sub(p[0] as u128);
@@ -20,10 +18,8 @@ pub(crate) const fn add(a: [u64; 4], b: [u64; 4], p: [u64; 4]) -> [u64; 4] {
 
     let s = l0 as u128 + (p[0] & brw) as u128;
     let (l0, c) = (s as u64, (s >> 64) as u64);
-    let s = l1 as u128 + (p[1] & brw) as u128 + c as u128;
-    let (l1, c) = (s as u64, (s >> 64) as u64);
-    let s = l2 as u128 + (p[2] & brw) as u128 + c as u128;
-    let (l2, c) = (s as u64, (s >> 64) as u64);
+    let (l1, c) = adc(l1, p[1] & brw, c);
+    let (l2, c) = adc(l2, p[2] & brw, c);
     let l3 = l3.wrapping_add(p[3] & brw).wrapping_add(c);
 
     [l0, l1, l2, l3]
@@ -39,10 +35,8 @@ pub(crate) const fn sub(a: [u64; 4], b: [u64; 4], p: [u64; 4]) -> [u64; 4] {
 
     let s = l0 as u128 + (p[0] & brw) as u128;
     let (l0, c) = (s as u64, (s >> 64) as u64);
-    let s = l1 as u128 + (p[1] & brw) as u128 + c as u128;
-    let (l1, c) = (s as u64, (s >> 64) as u64);
-    let s = l2 as u128 + (p[2] & brw) as u128 + c as u128;
-    let (l2, c) = (s as u64, (s >> 64) as u64);
+    let (l1, c) = adc(l1, p[1] & brw, c);
+    let (l2, c) = adc(l2, p[2] & brw, c);
     let l3 = l3.wrapping_add(p[3] & brw).wrapping_add(c);
 
     [l0, l1, l2, l3]
@@ -63,10 +57,8 @@ pub(crate) const fn double(a: [u64; 4], p: [u64; 4]) -> [u64; 4] {
 
     let s = l0 as u128 + (p[0] & brw) as u128;
     let (l0, c) = (s as u64, (s >> 64) as u64);
-    let s = l1 as u128 + (p[1] & brw) as u128 + c as u128;
-    let (l1, c) = (s as u64, (s >> 64) as u64);
-    let s = l2 as u128 + (p[2] & brw) as u128 + c as u128;
-    let (l2, c) = (s as u64, (s >> 64) as u64);
+    let (l1, c) = adc(l1, p[1] & brw, c);
+    let (l2, c) = adc(l2, p[2] & brw, c);
     let l3 = l3.wrapping_add(p[3] & brw).wrapping_add(c);
 
     [l0, l1, l2, l3]
@@ -227,7 +219,7 @@ pub(crate) const fn mont(a: [u64; 8], p: [u64; 4], inv: u64) -> [u64; 4] {
     let (l0, brw) = (s as u64, (s >> 64) as u64);
     let (l1, brw) = sbb(l5, p[1], brw);
     let (l2, brw) = sbb(l6, p[2], brw);
-    let (l3, brw) =  sbb(l7, p[3], brw);
+    let (l3, brw) = sbb(l7, p[3], brw);
 
     let s = l0 as u128 + (p[0] & brw) as u128;
     let (l0, c) = (s as u64, (s >> 64) as u64);
